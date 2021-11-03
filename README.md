80a0df2a27704e981d076b5635c1e9f8b68c9715
nothub <48992448+nothub@users.noreply.github.com>
2021-11-03 02:20:14 +0100

    💩

---
+++ README.md
@@ -0,0 +1,4 @@
+### servers by region
+(scraped from the games [status page](https://www.newworld.com/en-us/support/server-status))
+
+#### [latest servers.json](https://github.com/nothub/newworld-server-scraper/releases/latest/download/servers.json)
---
+++ main.py
@@ -0,0 +1,39 @@
+import json
+
+import requests
+from bs4 import BeautifulSoup
+
+
+def scrape_servers(region_id: int):
+    servers = list()
+    for region_data in soup.findAll(attrs={"data-index": region_id}):
+        region_servers = region_data.findAll("div", class_="ags-ServerStatus-content-responses-response-server-name")
+        for server in region_servers:
+            servers.append(server.text.strip())
+    return servers
+
+
+if __name__ == '__main__':
+
+    response = requests.get("https://www.newworld.com/support/server-status")
+    soup = BeautifulSoup(response.content, 'html.parser')
+
+    regions = dict()
+    for region in soup.find_all(name="a", attrs={"class": "ags-ServerStatus-content-tabs-tabHeading"}):
+        regions[region['data-index']] = region.text.strip()
+
+    if not len(regions):
+        print("no regions found, something is fucked!")
+        exit(1)
+
+    results = dict()
+    for region_id, region_name in regions.items():
+        results[region_name] = scrape_servers(region_id)
+        count = len(results[region_name])
+        if not count:
+            print("no servers in region " + region_name + ", something is fucked!")
+        else:
+            print(region_name + ": " + str(count) + " servers")
+
+    with open('servers.json', 'w') as output_file:
+        output_file.write(json.dumps(results, indent=2))
